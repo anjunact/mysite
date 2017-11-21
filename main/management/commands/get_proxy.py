2c0bc71ceae55f3b1c27d9fa283811b2509de52f
anjunact <anjunact@gmail.com>
2017-11-21 16:24:36 +0800

    'proxy'

--- main/management/commands/get_proxy.py
+++ main/management/commands/get_proxy.py
@@ -1,12 +1,18 @@
+import random
+from selenium.webdriver.common.proxy import ProxyType 
 from django.core.management.base import BaseCommand, CommandError
 from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+from django.db import connection
+from main.models import t_ip
 
 
 class Command(BaseCommand):
+    def add_arguments(self,parser):
+        parser.add_argument('check',nargs='+', type=str)
+
     def getData(self):
         url='http://www.xicidaili.com'
         driver = webdriver.PhantomJS()
@@ -15,17 +21,43 @@ class Command(BaseCommand):
         # print(elem)
         soup = BeautifulSoup(driver.page_source, 'lxml')
         trs = soup.find(id="ip_list").find_all('tr')
+        
         for tr in trs:
-            tds =tr.find_all("td")
-           
+            tds =tr.find_all("td")         
             i =0 ;
+            tip = t_ip()
             for td in tds:
                 if i==1 :
-                    print(td)
+                    tip.ip = td.text
                 if i==2:
-                    print(td)
+                    tip.port = int(td.text)
                 i=i+1
+            # print(tip)
+            if tip.ip !=None and tip.port != None:
+                tip.save()
+            
+    def check(self):
+        driver = webdriver.PhantomJS()      
+        proxy=webdriver.Proxy()
+        proxy.proxy_type= ProxyType.MANUAL
+        proxy.http_proxy='1.9.171.51:800'             
+        ips =self.allip()
+        i = random.randint(0,len(ips))
+        endport = ips[i]
+        proxy.http_proxy = endport.ip+':'+endport.port
+        driver.get("http://www.baidu.com")
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        title = soup.find_all('title')  
+        print(title)
         
- 
+    def allip(self):
+        return t_ip.objects.all()
     def handle(self, *args, **options):
-        self.getData()
+        check = options['check']
+        if(len(check)>0 and check[0]=='check'):  
+                
+            self.check()
+        else:
+            cursor= connection.cursor()
+            cursor.execute("truncate table main_t_ip")
+            self.getData()
--- main/models.py
+++ main/models.py
@@ -3,7 +3,9 @@ from django.db import models
 import django.utils.timezone as timezone
 # Create your models here.
 class t_ip(models.Model):
-    ip = models.CharField(max_length=15)
-    port=models.IntegerField()
+    ip = models.CharField(max_length=15,null=True,blank=True)
+    port=models.CharField(max_length=10,null=True,blank=True)
     created = models.DateTimeField('保存日期',default = timezone.now)
     updated = models.DateTimeField('最后修改日期', auto_now = True)
+    def __str__(self):
+        return "{},{}".format(self.ip,self.port)
